26db779f20c2ef0b613f51d9c2f76405eda22b13
dpill <a20141105059@mac05.local>
2016-06-20 10:48:48 +0800

    在程序执行过程中，其值不发生改变的量称为常量，其值可变的量称为变量,常量不能手动改数据。

---
+++ main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  1
+//
+//  Created by 20141105059c on 16/6/20.
+//  Copyright © 2016年 20141105059c. All rights reserved.
+//
+
+import Foundation
+
+var a=2
+var b=3
+let c=a+b
+print(c)
